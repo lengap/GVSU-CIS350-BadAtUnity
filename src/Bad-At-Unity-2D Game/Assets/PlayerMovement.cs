053f5ff19cf532a9d5ac451a5245b9e69e151ff0
Patrick Lenga <lengap@mail.gvsu.edu>
2020-09-21 14:58:48 -0400

    Added PlayerMovement Script to sprite

---
+++ src/Bad-At-Unity-2D Game/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
