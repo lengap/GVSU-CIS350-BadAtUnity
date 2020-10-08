8b2ee6b6017df3b45a3e3de99e461164a3f8af56
Patrick Lenga <lengap@mail.gvsu.edu>
2020-10-08 17:44:40 -0400

    Improved EnemyAI Program and implemented enemy shooting

--- src/Bad-At-Unity-2D Game/Assets/Scripts/Enemy/EnemyAI.cs
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public float speed;
+    public float stopDist;
+    public float retreatDist;
+
+    public Transform player;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, player.position) > stopDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stopDist && Vector2.Distance(transform.position, player.position) > retreatDist)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDist) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+
+
     }
 }
