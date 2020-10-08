ba94c583954ff129d30e5603d33e589d93f3bf85
Patrick Lenga <lengap@mail.gvsu.edu>
2020-10-08 09:55:38 -0400

    Changed Player Movement Script

--- src/Bad-At-Unity-2D Game/Assets/PlayerMovement.cs
+++ src/Bad-At-Unity-2D Game/Assets/PlayerMovement.cs
@@ -6,16 +6,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speed = 3.0f;
+    private float horizontal;
+    private float vertical;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * speed * Time.deltaTime;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 }
---
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform FirePoint; //This is where the bullet is created
+    public GameObject bulletPrefab; //prefab for the bullet sprite and physics
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Can change fire input in "Project input settings"
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+}
