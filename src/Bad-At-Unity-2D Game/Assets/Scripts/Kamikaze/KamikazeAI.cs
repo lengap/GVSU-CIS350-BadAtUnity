2f586e5944c9693454f90d2c7bcedb6542ef1132
Hunter <chopph@mail.gvsu.edu>
2020-11-04 23:31:31 -0500

    Added New boss, and started second

---
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Kamikaze/KamikazeAI.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class KamikazeAI : MonoBehaviour
+{
+    public float speed;
+    public float stopDist;
+    public float retreatDist;
+
+    private Transform player;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public Transform FirePoint;
+
+    public GameObject projectile1;
+    public GameObject projectile2;
+    public GameObject projectile3;
+    public GameObject deathEffect;
+
+    public int health = 100;
+
+    private int shotCount;
+    private int startShotCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+        shotCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if three shots have not been fired
+        if (shotCount < 3)
+        {
+            //if further than stop distance, move towards player
+            if (Vector2.Distance(transform.position, player.position) > stopDist)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            //if further than stop and further than retreat
+            else if (Vector2.Distance(transform.position, player.position) < stopDist && Vector2.Distance(transform.position, player.position) > retreatDist)
+            {
+                transform.position = this.transform.position;
+            }
+            else if (Vector2.Distance(transform.position, player.position) < retreatDist)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            }
+
+        }
+
+        //if three shots have been fired, and the next shot is not firing, move faster
+        else if (shotCount == 3 && timeBtwShots > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, (speed + 2) * Time.deltaTime);
+        }
+
+        else
+        {
+            shotCount = 0;
+        }
+
+        //if time to shoot
+        if (timeBtwShots <= 0)
+        {
+            
+
+            Instantiate(projectile1, FirePoint.position, Quaternion.identity);
+            Instantiate(projectile2, FirePoint.position, Quaternion.identity);
+            Instantiate(projectile3, FirePoint.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+            shotCount++;
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+
+
+    }
+
+    public void TakeDamage( int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Kamikaze/SGProjectiles/KP2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class KP2 : MonoBehaviour {
+
+    //this is the projectile aimed above the player
+
+    public float speed;
+    public int damage;
+
+    private Transform player;
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target= new Vector2(player.position.x , player.position.y+1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * (Time.deltaTime/10));
+
+        if (transform.position.x == target.x && transform.position.y == target.y) {
+            DestroyProjectile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other.name);
+        if (other.CompareTag("Player")) {
+            DestroyProjectile();
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                
+                player.TakeDamage(20);
+            }
+        }
+    }
+
+    void DestroyProjectile() {
+        Destroy(gameObject);
+    }
+
+
+
+}
