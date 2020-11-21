8c9a2b8ba3e0ce0377d2394d0ace25724a88bbd0
Patrick Lenga <lengap@mail.gvsu.edu>
2020-11-21 13:20:42 -0500

    Added new room and additional sprites

--- src/Bad-At-Unity-2D Game/Assets/Room_Loader.cs
+++ src/Bad-At-Unity-2D Game/Assets/Room_Loader.cs
@@ -23,15 +23,23 @@ public class Room_Loader : MonoBehaviour
             Player player = other.GetComponent<Player>();
             if (player != null)
             {
-				if(SceneManager.GetActiveScene().name == "RoomTest"){
+				if(SceneManager.GetActiveScene().name != "Level"){
 					SceneManager.LoadScene("Level");
-					SceneManager.UnloadScene("RoomTest");
+					SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
 					
 					Destroy(gameObject);
 				}
 				else if (SceneManager.GetActiveScene().name == "Level"){
-					SceneManager.LoadScene("RoomTest");
-					SceneManager.UnloadScene("Level");
+					int rand = Random.Range(0,2);
+					if(rand == 0){
+						SceneManager.LoadScene("Bedroom");
+						SceneManager.UnloadSceneAsync("Level");
+					}
+					if(rand == 1){
+						SceneManager.LoadScene("Office");
+						SceneManager.UnloadSceneAsync("Level");
+					}
+					
 				}
             }
         }
--- src/Bad-At-Unity-2D Game/Assets/Scripts/Enemy/EnemyTransport.cs
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Enemy/EnemyTransport.cs
@@ -8,10 +8,6 @@ public class EnemyTransport : MonoBehaviour
     public float playerDistance;
     public int health = 100;
     public Transform player;
-    public Transform SpawnPoint;
-    public GameObject[] EnemyType;
-    private float timeBtwSpawns;
-    public float startTimeBtwSpawns;
 
   
     public float speed;
@@ -34,22 +30,11 @@ public class EnemyTransport : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        timeBtwSpawns = startTimeBtwSpawns;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if((Vector2.Distance(transform.position, player.position) <= playerDistance )){
-			if (timeBtwSpawns <= 0)
-			{
-				Instantiate(EnemyType[Random.Range(0,2)], SpawnPoint.position, Quaternion.identity);
-				timeBtwSpawns = startTimeBtwSpawns;
-			}
-			else {
-				timeBtwSpawns -= Time.deltaTime;
-			}
-		}
 
         if (Vector2.Distance(transform.position, player.position) > stopDist)
         {
