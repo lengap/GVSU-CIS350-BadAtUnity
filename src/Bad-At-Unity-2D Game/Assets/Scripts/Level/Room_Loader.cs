ea864a00b5332f7ca7b3b56a1c536605b07dd29b
Patrick Lenga <lengap@mail.gvsu.edu>
2020-12-11 15:23:28 -0500

    Added Final SRS and Game .exe to GitHub

--- src/Bad-At-Unity-2D Game/Assets/Scripts/Level/Room_Loader.cs
+++ src/Bad-At-Unity-2D Game/Assets/Scripts/Level/Room_Loader.cs
@@ -7,7 +7,6 @@ public class Room_Loader : MonoBehaviour
 {
 	
 	private int spnrCnt = 0;
-	private bool OnBoss = true;
 	
 	
     // Start is called before the first frame update
@@ -20,8 +19,7 @@ public class Room_Loader : MonoBehaviour
     void Update()
     {
 		spnrCnt = GameObject.FindGameObjectsWithTag("Spawner").Length;
-		Debug.Log("Spawner Count " + spnrCnt);
-		Debug.Log("OnBoss " + OnBoss);
+		Debug.Log("Spawner Count: " + spnrCnt);
 		
 		if(spnrCnt == 0 && SceneManager.GetActiveScene().name == "Level"){
 			SceneManager.LoadScene("BossRoom");
@@ -31,7 +29,6 @@ public class Room_Loader : MonoBehaviour
 	
     //if player runs into ladder, change the game scene
 	void OnTriggerEnter2D(Collider2D other) {
-		OnBoss = false;
         Debug.Log(other.name);
         if (other.CompareTag("Player")) {
             Player player = other.GetComponent<Player>();
@@ -40,26 +37,21 @@ public class Room_Loader : MonoBehaviour
 				if(SceneManager.GetActiveScene().name != "Level"){
 					SceneManager.LoadScene("Level");
 					Debug.Log(SceneManager.GetActiveScene().name);
-					OnBoss = true;
-					
 					Destroy(gameObject);
 				}
 				else if (SceneManager.GetActiveScene().name == "Level"){
 					int rand = Random.Range(0,3);
 					if(rand == 0){
-						OnBoss = false;
 						SceneManager.LoadScene("Bedroom");
 						Debug.Log(SceneManager.GetActiveScene().name);
 						
 					}
 					if(rand == 1){
-						OnBoss = false;
 						SceneManager.LoadScene("Office");
 						Debug.Log(SceneManager.GetActiveScene().name);
 						
 					}
 					if(rand == 2){
-						OnBoss = false;
 						SceneManager.LoadScene("Library");
 						Debug.Log(SceneManager.GetActiveScene().name);
 						
